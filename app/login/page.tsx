4ba1ed9ebd84f8c73b5a2c1575890e01437fe807
diwakar <d.7484959595@gmail.com>
2025-02-01 18:20:28 +0530

    complete authentication

---
+++ app/login/page.tsx
@@ -0,0 +1,50 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Image from "next/image";
+import { auth, signIn } from "../utils/auth";
+import { SubmitButton } from "../components/SubmitButtons";
+import { redirect } from "next/navigation";
+
+export default async function login() {
+  const session = await auth()
+
+  if (session?.user) {
+    console.log(session)
+    redirect("/dashboard")
+  }
+  return (
+    <>
+      <div className="flex h-screen w-full items-center justify-center">
+        <Card className="max-w-sm">
+          <CardHeader>
+            <CardTitle className="text-2xl">Login</CardTitle>
+            <CardDescription>Enter your email below to login</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form
+              action={async (formData) => {
+                "use server";
+                await signIn("nodemailer",formData);
+              }}
+              className="flex flex-col gap-y-2"
+            >
+              <div className="flex flex-col gap-y-2">
+                <Label>Email</Label>
+                <Input name="email" type="email" required placeholder="hello@hello.com" />
+              </div>
+              <SubmitButton/>
+            </form>
+          </CardContent>
+        </Card>
+      </div>
+    </>
+  );
+}
